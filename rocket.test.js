58c2ce1421f73fce295ce77a9dea01ecf163a2a5
Stanley Sham <stanley.sham94@gmail.com>
2019-08-27 16:42:53 -0400

    completed assignment

--- rocket.test.js
+++ rocket.test.js
@@ -54,6 +54,61 @@ describe('Rocket', () => {
         expect(rocket.liftOff()).toBe(false);
       });
     });
-  });
+    describe('land', () => {
+      test('it should return false if the rocket is not flying', () => {
+        let rocket = new Rocket;
 
+        expect(rocket.land()).toBe(false);
+      });
+      test('it should return true if the rocket is j0', () => {
+        let rocket = new Rocket({flying: true});
+
+        expect(rocket.land()).toBe(true);
+      });
+    });
+    describe('status', () => {
+      test('if the rocket is not flying, it should return that the rocket is ready for liftoff', () =>{
+        let rocket = new Rocket;
+
+        expect(rocket.status()).toBe(`Rocket ${rocket.name} is ready for liftoff!`)
+      });
+      test('if the rocket is not flying, it should return that the rocket is already flying', () =>{
+        let rocket = new Rocket({flying: true});
+
+        expect(rocket.status()).toBe(`Rocket ${rocket.name} is flying through the sky!`)
+      });
+    });
+    describe('sendCodedSignal', () => {
+      test('if nothing is passed in, signalinfo should return "boop"', () => {
+        let rocket = new Rocket;
+
+        expect(rocket.sendCodedSignal()).toBe('boop');
+      });
+      test('if message code was less than 10, than the signal info should be beep', () => {
+        let rocket = new Rocket;
+
+        expect(rocket.sendCodedSignal(6)).toBe('beep');
+      });
+      test('if message code is below 10 and is flying should return "beep beep"', () => {
+        let rocket = new Rocket({flying: true});
+
+        expect(rocket.sendCodedSignal(6)).toBe('beep beep');
+      });
+      test('if message code isn\'t a number, it should return "boop" "beep" "beep"', () => {
+        let rocket = new Rocket;
+
+        expect(rocket.sendCodedSignal('hello')).toBe('boop beep beep');
+      });
+      test('if message code is a number above 10, it should return "boop beep beep"', () => {
+        let rocket = new Rocket;
+
+        expect(rocket.sendCodedSignal(11)).toBe('boop beep beep');
+      });
+      test('if message code is not a number or a number above 10 and the rocket is flying, it should return "boop boop boop"', () => {
+        let rocket = new Rocket({flying: true});
+
+        expect(rocket.sendCodedSignal(11)).toBe('boop boop boop')
+      });
+    });
+  });
 });
